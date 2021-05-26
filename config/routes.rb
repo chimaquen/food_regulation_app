75c11a5579140aeb2d198216051b9e3f1a5cec9e
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2021-05-26 12:37:27 +0900

    Add platform

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/notices', to: 'notices#index'
+  root to: 'notices#index'
   get '/notices/new', to: 'notices#new' 
   post '/notices', to: 'notices#create' 
   delete '/notices/:id', to: 'notices#destroy'
